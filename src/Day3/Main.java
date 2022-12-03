93010e0b960cfcd6760321933d51910542a19b73
Alfonso Ristorato <alfonsoristorato@gmail.com>
2022-12-03 13:49:53 +0000

    part2 day3

--- src/Day3/Main.java
+++ src/Day3/Main.java
@@ -18,7 +18,7 @@ public class Main {
         } catch (IOException e) {
             System.out.println(e.getCause());
         }
-
+        // PART1
         List<Character> dupes = new ArrayList<>();
         List<Integer> dupesIntEquivalent = new ArrayList<>();
 
@@ -40,6 +40,39 @@ public class Main {
 
         }
         for (Character character : dupes) {
+            if (Character.isLowerCase(character)) {
+                dupesIntEquivalent.add((int) character - (int) 'a' + 1);
+            } else {
+                dupesIntEquivalent.add(((int) character - (int) 'A' + 1) + 26);
+            }
+        }
+        System.out.println(dupesIntEquivalent.stream().mapToInt(Integer::intValue).sum());
+
+        // PART2
+        dupesIntEquivalent.clear();
+        List<Character> badge = new ArrayList<>();
+        for (int i = 0; i < inputArray.length; i += 3) {
+            Set<Character> rucksack1Set = new HashSet<>();
+            String rucksack2 = inputArray[i + 1];
+            String rucksack3 = inputArray[i + 2];
+            char[] rucksack2CharArray = rucksack2.toCharArray();
+            char[] rucksack3CharArray = rucksack3.toCharArray();
+            Arrays.sort(rucksack2CharArray);
+            Arrays.sort(rucksack3CharArray);
+            for (int x = 0; x < inputArray[i].length(); x++) {
+                rucksack1Set.add(inputArray[i].charAt(x));
+            }
+            for (Character rucksack1Char : rucksack1Set) {
+
+                if (Arrays.binarySearch(rucksack2CharArray, rucksack1Char) >= 0
+                        && Arrays.binarySearch(rucksack3CharArray, rucksack1Char) >= 0) {
+                    badge.add(rucksack1Char);
+                    break;
+                }
+            }
+
+        }
+        for (Character character : badge) {
             if (Character.isLowerCase(character)) {
                 dupesIntEquivalent.add((int) character - (int) 'a' + 1);
             } else {
