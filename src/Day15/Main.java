1a281628bc8c36a47aada51f4cf56c1053631fba
alfonsoristorato <alfonsoristorato@gmail.com>
2022-12-16 16:31:33 +0000

    day15 part2 different solution that doesn't go out of memory.

--- src/Day15/Main.java
+++ src/Day15/Main.java
@@ -12,7 +12,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Path filePath = Path.of("src/Day15/Day15Input.txt");
         List<String> input = new ArrayList<>();
 
@@ -26,16 +26,11 @@ public class Main {
         Map<Point, String> grid = new HashMap<>();
         Integer y = 2000000;
         for (String line : input) {
-            String sensorInput = line.split(":")[0];
-            String beaconInput = line.split(":")[1];
-            String sensorX = sensorInput.substring(sensorInput.indexOf("x=") + 2, sensorInput.indexOf(","));
-            String sensorY = sensorInput.substring(sensorInput.indexOf("y=") + 2);
-            String beaconX = beaconInput.substring(beaconInput.indexOf("x=") + 2, beaconInput.indexOf(","));
-            String beaconY = beaconInput.substring(beaconInput.indexOf("y=") + 2);
+            Point[] sensorAndBeacon = buildPoints(line);
 
-            Point sensor = new Point(Integer.parseInt(sensorX), Integer.parseInt(sensorY));
+            Point sensor = sensorAndBeacon[0];
             pointsList.add(sensor);
-            Point beacon = new Point(Integer.parseInt(beaconX), Integer.parseInt(beaconY));
+            Point beacon = sensorAndBeacon[1];
             pointsList.add(beacon);
             grid.put(sensor, "S");
             grid.put(beacon, "B");
@@ -45,9 +40,7 @@ public class Main {
 
             Integer yDiff = Math.abs(sensor.y - beacon.y);
             Integer xDiff = Math.abs(sensor.x - beacon.x);
-            Integer yMin = Math.min(sensor.y, beacon.y);
-            Integer yMax = Math.max(sensor.y, beacon.y);
-            Integer totalDiff = yDiff + xDiff; // apparently this is called manatthan distance
+            Integer totalDiff = yDiff + xDiff;
             Integer remaining;
 
             if (sensor.y > y) {
@@ -73,77 +66,57 @@ public class Main {
         }
         System.out.println("Part 1: " + invalidPoints.size());
 
-        pointsList.clear();
-        invalidPoints.clear();
-        for (String line : input) {
-            String sensorInput = line.split(":")[0];
-            String beaconInput = line.split(":")[1];
-            String sensorX = sensorInput.substring(sensorInput.indexOf("x=") + 2, sensorInput.indexOf(","));
-            String sensorY = sensorInput.substring(sensorInput.indexOf("y=") + 2);
-            String beaconX = beaconInput.substring(beaconInput.indexOf("x=") + 2, beaconInput.indexOf(","));
-            String beaconY = beaconInput.substring(beaconInput.indexOf("y=") + 2);
+        int max = 4000000;
+        for (y = 0; y < max; y++) {
 
-            Point sensor = new Point(Integer.parseInt(sensorX), Integer.parseInt(sensorY));
-            pointsList.add(sensor);
-            Point beacon = new Point(Integer.parseInt(beaconX), Integer.parseInt(beaconY));
-            pointsList.add(beacon);
-        }
-        int maxX, maxY;
-        maxX = maxY = 4000000;
-        int minX, minY;
-        minX = minY = 0;
+            int x = 0;
 
-        for (int i = 0; i < pointsList.size(); i += 2) {
-            Point sensor = pointsList.get(i);
-            Point beacon = pointsList.get(i + 1);
-            grid.put(sensor, "S");
-            grid.put(beacon, "B");
-            Integer yDiff = Math.abs(sensor.y - beacon.y);
-            Integer xDiff = Math.abs(sensor.x - beacon.x);
-            int totalDiff = yDiff + xDiff; // apparently this is called manatthan distance
+            while (x <= max) {
 
-            int sesorBeaconYMax = Math.max(sensor.y, beacon.y);
-            int sesorBeaconYMin = Math.min(sensor.y, beacon.y);
-
-            int goingDown = 0;
-            System.out.println("line: " + i);
-            for (int y2 = minY; y2 < maxY; y2++) {
-
-                if (y2 < (sensor.y - totalDiff)) continue;
-                if (y2 > (sensor.y + totalDiff)) continue;
-                if(y2 == 0 && y2 > (sensor.y - totalDiff)){
-                    goingDown = sesorBeaconYMax - sesorBeaconYMin -1;
+                int newX = x;
+                for (String line : input) {
+                    Point[] sensorAndBeacon = buildPoints(line);
+                    newX = moveToEndOfLine(sensorAndBeacon[0], sensorAndBeacon[1], x, y);
+                    if (x != newX)
+                        break;
                 }
-                for (int x = minX; x <= goingDown; x++) {
-                        if (!grid.containsKey(new Point(x + sensor.x, y2))) {
-                            Point pointRight = new Point(x + sensor.x, y2);
-                            invalidPoints.add(pointRight);
-                        }
-
-                        if (!grid.containsKey(new Point((sensor.x - goingDown) + x, y2))) {
-                            Point pointLeft = new Point((sensor.x - goingDown) + x, y2);
-                            invalidPoints.add(pointLeft);
-                        }
-
+                if (newX == x) {
+                    long result = ((long)x * max) + y;
+                    System.out.println("Part 2: " + result);
+                    y = max + 1;
+                    break;
                 }
-                if(y2 < sensor.y){
-                    goingDown++;
-                }else{
-                    goingDown--;
-                }
-
-
-            }}
-
-        for (var x : invalidPoints) {
-            if (x.y == 11) {
-                // 2000000
-                // 1000000
-                System.out.println(x);
+                x = newX;
             }
         }
+    }
 
+    static Point[] buildPoints(String line) {
+        String sensorInput = line.split(":")[0];
+        String beaconInput = line.split(":")[1];
+        String sensorX = sensorInput.substring(sensorInput.indexOf("x=") + 2, sensorInput.indexOf(","));
+        String sensorY = sensorInput.substring(sensorInput.indexOf("y=") + 2);
+        String beaconX = beaconInput.substring(beaconInput.indexOf("x=") + 2, beaconInput.indexOf(","));
+        String beaconY = beaconInput.substring(beaconInput.indexOf("y=") + 2);
+        Point sensor = new Point(Integer.parseInt(sensorX), Integer.parseInt(sensorY));
+        Point beacon = new Point(Integer.parseInt(beaconX), Integer.parseInt(beaconY));
+        return new Point[]{sensor, beacon};
+    }
 
+    static int moveToEndOfLine(Point sensor, Point beacon, int x, int y) {
+        Integer yDiff = Math.abs(sensor.y - beacon.y);
+        Integer xDiff = Math.abs(sensor.x - beacon.x);
+        Integer totalDiff = yDiff + xDiff;
+        int remaining;
+        if (sensor.y > y) {
+            remaining = totalDiff - (sensor.y - y);
+        } else {
+            remaining = totalDiff - (y - sensor.y);
+        }
+        if (x >= sensor.x - remaining && x <= sensor.x + remaining) {
+            return sensor.x + remaining + 1;
+        }
+        return x;
 
     }
 
